6d092b6823778837d908b3805e2b176a0c90ec96
Bruno <martin.bruno.mail@gmail.com>
2019-11-24 22:49:04 +0100

    red inner loop ; agent list

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -8,3 +8,9 @@ def test_ft_parsing():
     assert len(cal_ics["CLEMENT"].events) == 2
     assert cal_ics["CLEMENT"][0].day == date(2019, 11, 22)
     assert cal_ics["CLEMENT"][0].txt_shift == "8h-9h30"
+
+
+def test_ut_get_list_agents():
+    cal_ics = Ics.from_csv("./fixture_1.csv")
+
+    assert cal_ics.agents == ("MARIE", "JOE", "CLEMENT")
