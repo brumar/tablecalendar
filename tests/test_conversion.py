730153b7d684b32f5a2f26e381a2b23b631fac26
Bruno <martin.bruno.mail@gmail.com>
2019-11-24 23:08:24 +0100

    red inner loop ; better filepath

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -3,7 +3,7 @@ from datetime import date
 
 
 def test_ft_parsing():
-    cal_ics = Ics.from_csv("./fixture_1.csv")
+    cal_ics = Ics.from_csv("./tests/fixture_1.csv")
 
     assert len(cal_ics["CLEMENT"].events) == 2
     assert cal_ics["CLEMENT"][0].day == date(2019, 11, 22)
@@ -11,6 +11,6 @@ def test_ft_parsing():
 
 
 def test_ut_get_list_agents():
-    cal_ics = Ics.from_csv("./fixture_1.csv")
+    cal_ics = Ics.from_csv("./tests/fixture_1.csv")
 
     assert cal_ics.agents == ("MARIE", "JOE", "CLEMENT")
