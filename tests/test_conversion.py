7e52a2b601b454f6dbd038f925ba1b753353ff25
Bruno <martin.bruno.mail@gmail.com>
2019-11-25 00:37:37 +0100

    green inner loop ; refactor test by creating a fixture

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,21 +1,24 @@
 from icsmaker import Ics, AgentEvents
 from datetime import date
+from pytest import fixture
 
 
-def test_ft_parsing():
-    cal_ics = Ics.from_csv("./tests/fixture_1.csv")
+@fixture(name="cal_ics")
+def cal_ics():
+    yield Ics.from_csv("./tests/fixture_1.csv")
+
+
+def test_ft_parsing(cal_ics):
 
     assert len(cal_ics["CLEMENT"].events) == 2
     assert cal_ics["CLEMENT"][0].day == date(2019, 11, 22)
     assert cal_ics["CLEMENT"][0].txt_shift == "8h-9h30"
 
 
-def test_ut_get_list_agents():
-    cal_ics = Ics.from_csv("./tests/fixture_1.csv")
+def test_ut_get_list_agents(cal_ics):
 
     assert cal_ics.agents == ("MARIE", "JOE", "CLEMENT")
 
 
-def test_ut_get_agent():
-    cal_ics = Ics.from_csv("./tests/fixture_1.csv")
+def test_ut_get_agent(cal_ics):
     assert isinstance(cal_ics["MARIE"], AgentEvents)
