de55bb080db624c4186e967d1084bab1e01b5129
Bruno <martin.bruno.mail@gmail.com>
2019-11-24 22:51:38 +0100

    still red outer loop ; local packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tablecalendar",
+    version="1.0.0",
+    url="https://github.com/brumar/tablecalendar.git",
+    author="Bruno Martin",
+    author_email="martin.bruno.mail@gmail.com",
+    description="Description of my package",
+    packages=find_packages(),
+    install_requires=[""],
+)
