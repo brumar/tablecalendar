0476a79af6907bae4cdd922a5ef30f1063c12fe7
Bruno <martin.bruno.mail@gmail.com>
2019-11-25 10:39:06 +0100

    green outer loop ; refactoring (2)

--- icsmaker.py
+++ icsmaker.py
@@ -34,13 +34,22 @@ class Ics:
             first_row = next(reader)
             agents = tuple(first_row[2:])
             ics = Ics(agents)
-            for row in reader:
-                event = Event.from_rawstrings(row[0], row[1])
-                for c, cell in enumerate(row[2:]):
-                    if cell == "x":
-                        agent = ics.agents[c]
-                        ics[agent].events.append(event)
+            ics.add_events_from_csvreader(reader)
             return ics
+
+    def add_events_from_csvreader(self, reader):
+        for row in reader:
+            event = Event.from_rawstrings(row[0], row[1])
+            agents = self.get_agents_from_row(row[2:])
+            for agent in agents:
+                self[agent].events.append(event)
+
+    def get_agents_from_row(self, x_list):
+        agents = []
+        for c, cell in enumerate(x_list):
+            if cell == "x":
+                agents.append(self.agents[c])
+        return agents
 
     def __getitem__(self, item):
         return self.agents_dict[item]
